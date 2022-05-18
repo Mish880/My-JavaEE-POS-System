940290505385560e563d0b55a39997c3d2ef6b0e
Mish880 <Pereraittec6@gmail.com>
2022-05-18 12:23:28 +0530

    Update Business layer

--- JAVAEEPOS/src/controller/CustomerServlet.java
+++ JAVAEEPOS/src/controller/CustomerServlet.java
@@ -1,4 +1,35 @@
 package controller;
 
-public class CustomerServlet {
+import dao.CrudDao;
+import dao.DAOFactory;
+import entity.Customer;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    CrudDao<Customer,String> customerDAO = DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.CUSTOMER);
+
+    @Override
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            resp.setContentType("application/json");
+
+            resp.getWriter().print(customerDAO.getAll());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+   
+
 }
