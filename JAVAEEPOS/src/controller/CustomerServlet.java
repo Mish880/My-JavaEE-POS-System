eb2da4c8f0351b0539edfadf5bc0e5ac1f4f526a
Mish880 <Pereraittec6@gmail.com>
2022-05-19 11:05:24 +0530

    Update Customer Serverlet Item Serverlet

--- JAVAEEPOS/src/controller/CustomerServlet.java
+++ JAVAEEPOS/src/controller/CustomerServlet.java
@@ -4,6 +4,8 @@ import dao.CrudDao;
 import dao.DAOFactory;
 import entity.Customer;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,6 +32,52 @@ public class CustomerServlet extends HttpServlet {
         }
     }
 
-   
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        try{
+            Customer customer = new Customer(req.getParameter("txtCustomerAddId"), req.getParameter("txtCustomerAddName"),req.getParameter("txtCustomerAddAddress"),req.getParameter("txtCustomerAddContact"));
+            customerDAO.add(customer);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+  }
 
+ @Override
+ protected void doPut(HttpServletRequest req, HttpServletResponse resp){
+        try{
+            JsonObject customerJson = Json.createReader(req.getReader()).readObject();
+            Customer customer = new Customer(customerJson.getString("id"), customerJson.getString("name"),customerJson.getString("address"),customerJson.getString("contact"));
+
+            if (customerDAO.update(customer)) {
+
+            }else {
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+ }
+
+   @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp){
+
+        try{
+            if (customerDAO.delete(req.getParameter("customerId"))){
+
+            } else {
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+   }
 }
--- JAVAEEPOS/src/controller/ItemServlet.java
+++ JAVAEEPOS/src/controller/ItemServlet.java
@@ -1,4 +1,14 @@
 package controller;
 
-public class ItemServlet {
+
+import dao.CrudDao;
+import dao.DAOFactory;
+import dto.itemDto;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/item")
+public class ItemServlet extends HttpServlet {
+     CrudDao<itemDto, String> itemDAO = DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
 }
