89afd87960bb78585a6c932d064ebe51a0445712
Mish880 <Pereraittec6@gmail.com>
2022-05-24 11:59:22 +0530

    Update order models

--- JAVAEEPOS/src/entity/Order.java
+++ JAVAEEPOS/src/entity/Order.java
@@ -1,4 +1,60 @@
 package entity;
 
 public class Order {
+      private String oid;
+      private String cusid;
+      private String date;
+      private String time;
+      private double cost;
+
+    public Order() {
+    }
+
+    public Order(String oid, String cusid, String date, String time, double cost) {
+        this.setOid(oid);
+        this.setCusid(cusid);
+        this.setDate(date);
+        this.setTime(time);
+        this.setCost(cost);
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getCusid() {
+        return cusid;
+    }
+
+    public void setCusid(String cusid) {
+        this.cusid = cusid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
--- JAVAEEPOS/src/entity/Orderdetails.java
+++ JAVAEEPOS/src/entity/Orderdetails.java
@@ -1,4 +1,50 @@
 package entity;
 
 public class Orderdetails {
+       private String itemid;
+       private String oid;
+       private int qty;
+       private double price;
+
+    public Orderdetails() {
+    }
+
+    public Orderdetails(String itemid, String oid, int qty, double price) {
+        this.setItemid(itemid);
+        this.setOid(oid);
+        this.setQty(qty);
+        this.setPrice(price);
+    }
+
+    public String getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(String itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
