de3a8569240029cf1fc46a2433f3396ca4b38fce
Mish880 <Pereraittec6@gmail.com>
2022-06-08 09:44:32 +0530

    Update order order detail

--- JAVAEEPOS/src/dto/orderDto.java
+++ JAVAEEPOS/src/dto/orderDto.java
@@ -1,4 +1,60 @@
 package dto;
 
 public class orderDto {
+     private String orderId;
+     private String ordercusId;
+     private String orderdate;
+     private String ordertime;
+     private double ordercost;
+
+    public orderDto() {
+    }
+
+    public orderDto(String orderId, String ordercusId, String orderdate, String ordertime, double ordercost) {
+        this.setOrderId(orderId);
+        this.setOrdercusId(ordercusId);
+        this.setOrderdate(orderdate);
+        this.setOrdertime(ordertime);
+        this.setOrdercost(ordercost);
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrdercusId() {
+        return ordercusId;
+    }
+
+    public void setOrdercusId(String ordercusId) {
+        this.ordercusId = ordercusId;
+    }
+
+    public String getOrderdate() {
+        return orderdate;
+    }
+
+    public void setOrderdate(String orderdate) {
+        this.orderdate = orderdate;
+    }
+
+    public String getOrdertime() {
+        return ordertime;
+    }
+
+    public void setOrdertime(String ordertime) {
+        this.ordertime = ordertime;
+    }
+
+    public double getOrdercost() {
+        return ordercost;
+    }
+
+    public void setOrdercost(double ordercost) {
+        this.ordercost = ordercost;
+    }
 }
--- JAVAEEPOS/src/dto/orderdetailDto.java
+++ JAVAEEPOS/src/dto/orderdetailDto.java
@@ -1,4 +1,50 @@
 package dto;
 
 public class orderdetailDto {
+    private String oid;
+    private String itemid;
+    private int qty;
+    private double price;
+
+    public orderdetailDto() {
+    }
+
+    public orderdetailDto(String oid, String itemid, int qty, double price) {
+        this.setOid(oid);
+        this.setItemid(itemid);
+        this.setQty(qty);
+        this.setPrice(price);
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(String itemid) {
+        this.itemid = itemid;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
